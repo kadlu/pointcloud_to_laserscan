32ebbf86dacf0530b295bcb2c2db568f6bbfe477
kadlu <50589471+kadlu@users.noreply.github.com>
2022-08-30 14:07:09 +0200

    Update scan_to_cloud_launch.py

--- launch/scan_to_cloud_launch.py
+++ launch/scan_to_cloud_launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
             package='pointcloud_to_laserscan',
             executable='laserscan_to_pointcloud_node',
             name='laserscan_to_pointcloud',
-            remappings=[('scan_in', [LaunchConfiguration(), '/scan']),
+            remappings=[('scan_in', ['/scan', '/scan']),
                         ('cloud', [LaunchConfiguration(variable_name='scanner'), '/cloud'])],
             parameters=[{'target_frame': 'laser', 'transform_tolerance': 0.01}]
         ),
